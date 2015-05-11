b4817d8c32878b2a274534b46fd01bdcbadb77b9
Dmitri Zagidulin <dzagidulin@gmail.com>
2015-05-11 12:34:08 -0400

    Refactor Index controller to use the explorer service

--- priv/ember_riak_explorer/app/routes/index.js
+++ priv/ember_riak_explorer/app/routes/index.js
@@ -3,21 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
     model: function() {
-
-        var url = '/explore/clusters/default/nodes';
-        var result = Ember.$.ajax({ url: url });  // returns a Promise obj
-        return result.then(
-            // Success
-            function(data) {
-                return data;
-            },
-            // Error
-            function(error) {
-                console.log('Error fetching nodes: ' + error);
-                return {
-                    nodes: []
-                };
-            }
-        );
+        return this.explorer.getNodes('default');
     }
 });
--- priv/ember_riak_explorer/app/services/explorer.js
+++ priv/ember_riak_explorer/app/services/explorer.js
@@ -1,4 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+    getClusters: function() {
+        var url = '/explore/clusters/';
+        var result = Ember.$.ajax({ url: url });  // returns a Promise obj
+        return result.then(
+            // Success
+            function(data) {
+                return data;
+            },
+            // Error
+            function(error) {
+                console.log('Error fetching clusters: ' + error);
+                return {
+                    clusters: []
+                };
+            }
+        );
+    },
+
+    getNodes: function(cluster_id) {
+        var url = '/explore/clusters/'+ cluster_id + '/nodes';
+        var result = Ember.$.ajax({ url: url });  // returns a Promise obj
+        return result.then(
+            // Success
+            function(data) {
+                return data;
+            },
+            // Error
+            function(error) {
+                console.log('Error fetching nodes: ' + error);
+                return {
+                    nodes: []
+                };
+            }
+        );
+    }
 });
