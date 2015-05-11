e2889e565b785e52528573994c835d13ed9f6c78
Dmitri Zagidulin <dzagidulin@gmail.com>
2015-05-11 15:20:13 -0400

    Change Index page to use cluster discovery
    - Implement explorer service function getClustersAndNodes()
    - Change the index template to use it
    
    Addresses issue #31

--- priv/ember_riak_explorer/app/routes/index.js
+++ priv/ember_riak_explorer/app/routes/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
     model: function() {
-        return this.explorer.getNodes('default');
+        return this.explorer.getClustersAndNodes();
     }
 });
--- priv/ember_riak_explorer/app/services/explorer.js
+++ priv/ember_riak_explorer/app/services/explorer.js
@@ -1,39 +1,57 @@
 import Ember from 'ember';
 
-export default Ember.Service.extend({
-    getClusters: function() {
-        var url = '/explore/clusters/';
-        var result = Ember.$.ajax({ url: url });  // returns a Promise obj
-        return result.then(
-            // Success
-            function(data) {
-                return data;
-            },
-            // Error
-            function(error) {
-                console.log('Error fetching clusters: ' + error);
-                return {
-                    clusters: []
-                };
-            }
-        );
-    },
+function getClusters() {
+    var url = '/explore/clusters/';
+    var result = Ember.$.ajax({ url: url });  // returns a Promise obj
+    return result.then(
+        // Success
+        function(data) {
+            return data.clusters;
+        },
+        // Error
+        function(error) {
+            console.log('Error fetching clusters: ' + error);
+            return [];
+        }
+    );
+}
 
-    getNodes: function(cluster_id) {
-        var url = '/explore/clusters/'+ cluster_id + '/nodes';
-        var result = Ember.$.ajax({ url: url });  // returns a Promise obj
-        return result.then(
-            // Success
-            function(data) {
-                return data;
-            },
-            // Error
-            function(error) {
-                console.log('Error fetching nodes: ' + error);
-                return {
-                    nodes: []
-                };
-            }
-        );
+function getNodes(cluster_id) {
+    var url = '/explore/clusters/'+ cluster_id + '/nodes';
+    var result = Ember.$.ajax({ url: url });  // returns a Promise obj
+    return result.then(
+        // Success
+        function(data) {
+            return data.nodes;
+        },
+        // Error
+        function(error) {
+            console.log('Error fetching nodes: ' + error);
+            return [];
+        }
+    );
+}
+
+export default Ember.Service.extend({
+    // Return all clusters that Explorer knows about
+    getClusters: getClusters,
+
+    // Return all nodes for a particular cluster
+    getNodes: getNodes,
+
+    // Return a list of clusters with their nodes, in the form:
+    // [ { id: 'default', props: [], nodes: [ <list of nodes> ] }, ... ]
+    getClustersAndNodes: function() {
+        return getClusters().then(function(clusters_list) {
+            return Ember.RSVP.all(
+                clusters_list.map(function(cluster) {
+                    return new Ember.RSVP.hash({
+                        id: cluster.id,
+                        props: cluster.props,
+                        nodes: getNodes(cluster.id)
+                    });
+                })
+            );
+        });
     }
 });
